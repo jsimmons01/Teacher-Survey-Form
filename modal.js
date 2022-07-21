e94c8f28adc7a7b8c24349861b54cd9616f2af6d
Jahmella <81432170+jsimmons01@users.noreply.github.com>
2022-07-21 14:12:51 -0400

    Update modal.js

--- modal.js
+++ modal.js
@@ -9,6 +9,10 @@ var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
+   setTimeout(function() {
+    modal.close();
+  }, 5000);
+
 }
 span.onclick = function() {
   modal.style.display = "none";
