14591aab84fd634d2a7f42a4ddb33612638e46b6
Jahmella Williams <simmons.jahmella@gmail.com>
2023-07-31 21:42:26 -0400

    Closes modal

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const submit = document.getElementById("submit");
-const modal = document.querySelector(".modal")
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close");
+
 
 submit.addEventListener("click", (e)=> {
     e.preventDefault();
 
     modal.style.display = 'block';
 
+
+});
+
+closeModal.addEventListener('click', ()=> {
+    modal.style.display = "none"
 })
